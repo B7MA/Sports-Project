de03ff8243ecffc18c3daad44a8395526dc35b91
B7MA <mhmdalmwdy895@gmail.com>
2026-08-31 03:45:34 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // عدّل هذين السطرين بقيمك من Supabase (Settings → API)
-const SUPABASE_URL = "ضع_رابط_مشروعك_هنا";
-const SUPABASE_ANON_KEY = "ضع_المفتاح_العام_هنا";
+const SUPABASE_URL = "https://iinghnzfflnzgmwoxalj.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_pxK8vAxYilGZjSTrjHQ-aQ_EMhdTSKy";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
